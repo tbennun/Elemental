8771bc7d4f31187c57e53f6a1dad5ada9391980d
Thomas R. Benson <benson31@llnl.gov>
2018-11-28 13:28:48 -0800

    finish cmake support for vtune and nvprof

---
+++ cmake/modules/FindNVTX.cmake
@@ -0,0 +1,30 @@
+# Sets the following variables
+#
+#   NVTX_FOUND
+#   NVTX_LIBRARY
+#
+# Defines the following imported target:
+#
+#   cuda::nvtx
+#
+
+find_library(NVTX_LIBRARY nvToolsExt
+  HINTS ${NVTX_DIR} $ENV{NVTX_DIR} ${CUDA_TOOLKIT_ROOT_DIR} ${CUDA_SDK_ROOT_DIR}
+  PATH_SUFFIXES lib64 lib
+  DOC "The nvtx library."
+  NO_DEFAULT_PATH)
+find_library(NVTX_LIBRARY nvToolsExt)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(NVTX
+  DEFAULT_MSG NVTX_LIBRARY)
+
+add_library(cuda::nvtx INTERFACE IMPORTED)
+
+if (NVTX_FOUND)
+  set_property(TARGET cuda::nvtx PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES "${CUDA_INCLUDE_DIRS}")
+
+  set_property(TARGET cuda::nvtx PROPERTY
+    INTERFACE_LINK_LIBRARIES "${NVTX_LIBRARY}")
+endif (NVTX_FOUND)
---
+++ cmake/modules/FindVTUNE.cmake
@@ -0,0 +1,36 @@
+# Exports the following variables
+#
+#   VTUNE_INCLUDE_PATH
+#   VTUNE_LIBRARY
+#
+# The following IMPORTED target is also created:
+#
+#   vtune::vtune
+#
+find_path(VTUNE_INCLUDE_PATH ittnotify.h
+  HINTS ${VTUNE_DIR} $ENV{VTUNE_DIR}
+  PATH_SUFFIXES include
+  NO_DEFAULT_PATH
+  DOC "The location of VTune headers.")
+find_path(VTUNE_INCLUDE_PATH ittnotify.h)
+
+find_library(VTUNE_LIBRARY NAMES libittnotify.a ittnotify
+  HINTS ${VTUNE_DIR} $ENV{VTUNE_DIR}
+  PATH_SUFFIXES lib64 lib
+  NO_DEFAULT_PATH
+  DOC "The location of VTune Static lib")
+find_library(VTUNE_LIBRARY NAMES libittnotify.a ittnotify)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(VTUNE
+  DEFAULT_MSG VTUNE_LIBRARY VTUNE_INCLUDE_PATH)
+
+add_library(vtune::vtune INTERFACE IMPORTED)
+
+if (VTUNE_FOUND)
+  set_property(TARGET vtune::vtune PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES "${VTUNE_INCLUDE_PATH}")
+
+  set_property(TARGET vtune::vtune PROPERTY
+    INTERFACE_LINK_LIBRARIES "${VTUNE_LIBRARY}")
+endif (VTUNE_FOUND)
