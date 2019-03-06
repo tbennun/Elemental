01e38223d88dd399b142cd33240902181036cdec
Nikoli Dryden <dryden1@llnl.gov>
2019-03-05 16:14:06 -0800

    Make memory pooled host memory default.
    This swaps the mode numbers with the non-pooled version to make integration simpler.

--- include/El/core/Memory/impl.hpp
+++ include/El/core/Memory/impl.hpp
@@ -30,9 +30,17 @@ G* New(size_t size, unsigned int mode, SyncInfo<Device::CPU> const&)
 {
     G* ptr = nullptr;
     switch (mode) {
-    case 0: ptr = new G[size]; break;
+    case 0:
+        ptr = static_cast<G*>(HostMemoryPool().Allocate(size * sizeof(G)));
+        break;
 #ifdef HYDROGEN_HAVE_CUDA
     case 1:
+        ptr = static_cast<G*>(PinnedHostMemoryPool().Allocate(size * sizeof(G)));
+        break;
+#endif // HYDROGEN_HAVE_CUDA
+    case 2: ptr = new G[size]; break;
+#ifdef HYDROGEN_HAVE_CUDA
+    case 3:
     {
         // Pinned memory
         auto error = cudaMallocHost(&ptr, size * sizeof(G));
@@ -44,14 +52,6 @@ G* New(size_t size, unsigned int mode, SyncInfo<Device::CPU> const&)
     }
     break;
 #endif // HYDROGEN_HAVE_CUDA
-    case 2:
-        ptr = static_cast<G*>(HostMemoryPool().Allocate(size * sizeof(G)));
-        break;
-#ifdef HYDROGEN_HAVE_CUDA
-    case 3:
-        ptr = static_cast<G*>(PinnedHostMemoryPool().Allocate(size * sizeof(G)));
-        break;
-#endif // HYDROGEN_HAVE_CUDA
     default: RuntimeError("Invalid CPU memory allocation mode");
     }
     return ptr;
@@ -61,9 +61,13 @@ template <typename G>
 void Delete( G*& ptr, unsigned int mode, SyncInfo<Device::CPU> const& )
 {
     switch (mode) {
-    case 0: delete[] ptr; break;
+    case 0: HostMemoryPool().Free(ptr); break;
 #ifdef HYDROGEN_HAVE_CUDA
-    case 1:
+    case 1: PinnedHostMemoryPool().Free(ptr); break;
+#endif  // HYDROGEN_HAVE_CUDA
+    case 2: delete[] ptr; break;
+#ifdef HYDROGEN_HAVE_CUDA
+    case 3:
     {
         // Pinned memory
         auto error = cudaFreeHost(ptr);
@@ -75,10 +79,6 @@ void Delete( G*& ptr, unsigned int mode, SyncInfo<Device::CPU> const& )
     }
     break;
 #endif // HYDROGEN_HAVE_CUDA
-    case 2: HostMemoryPool().Free(ptr); break;
-#ifdef HYDROGEN_HAVE_CUDA
-    case 3: PinnedHostMemoryPool().Free(ptr); break;
-#endif  // HYDROGEN_HAVE_CUDA
     default: RuntimeError("Invalid CPU memory deallocation mode");
     }
     ptr = nullptr;
