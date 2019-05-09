ae00b5b727b0847f46f264eb5fc2b39c02c83bca
Thomas R. Benson <benson31@llnl.gov>
2019-05-08 18:40:07 -0700

    add the cuda error code to the exception message

--- include/El/core/imports/cuda.hpp
+++ include/El/core/imports/cuda.hpp
@@ -26,7 +26,8 @@ struct CudaError : std::runtime_error
     {
         std::ostringstream oss;
         oss << ( async ? "Asynchronous CUDA error" : "CUDA error" )
-            << " (" << file << ":" << line << "): "
+            << " (error code: " << cuda_error << ") "
+            << "(" << file << ":" << line << "): "
             << cudaGetErrorString(cuda_error);
         return oss.str();
     }
