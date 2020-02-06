74cc427920f4d541fe1dfc667ae5755491379d3c
Tom Benson <30674819+benson31@users.noreply.github.com>
2020-02-05 17:00:26 -0800

    Improve OMP support on OSX (#94)

--- cmake/modules/FindAndVerifyOpenMP.cmake
+++ cmake/modules/FindAndVerifyOpenMP.cmake
@@ -3,53 +3,58 @@ include(CheckCXXSourceCompiles)
 # Attempt to use the built-in module
 find_package(OpenMP COMPONENTS CXX)
 
-if (NOT OpenMP_FOUND AND APPLE)
-
+if (NOT OpenMP_FOUND AND CMAKE_CXX_COMPILER_ID MATCHES "[Cc]lang")
   find_library(_OpenMP_LIBRARY
     NAMES omp gomp iomp5md
     HINTS ${OpenMP_DIR} $ENV{OpenMP_DIR}
-    PATH_SUFFIXES lib lib64
+    PATH_SUFFIXES lib64 lib
     NO_DEFAULT_PATH
     DOC "The libomp library.")
+  find_library(_OpenMP_LIBRARY
+    NAMES omp gomp iomp5md)
   mark_as_advanced(_OpenMP_LIBRARY)
 
   if (NOT _OpenMP_LIBRARY)
-    message(WARNING "No OpenMP library found.")
+    message(FATAL_ERROR "No OpenMP library found.")
   else ()
+
     get_filename_component(_OpenMP_LIB_DIR "${_OpenMP_LIBRARY}" DIRECTORY)
 
-    if (${_OpenMP_LIBRARY} MATCHES "libomp*")
+    if (${_OpenMP_LIBRARY} MATCHES "libomp")
       set(OpenMP_libomp_LIBRARY ${_OpenMP_LIBRARY}
         CACHE PATH "The OpenMP omp library.")
-      foreach (lang IN ITEMS C CXX)
-        set(OpenMP_${lang}_LIB_NAMES libomp)
-        set(OpenMP_${lang}_FLAGS "-fopenmp=libomp")
-      endforeach ()
-    elseif (${_OpenMP_LIBRARY} MATCHES "libgomp*")
+      set(OpenMP_CXX_LIB_NAMES omp)
+      set(OpenMP_CXX_FLAGS "-fopenmp=libomp")
+      set(OpenMP_omp_LIBRARY "${_OpenMP_LIBRARY}")
+    elseif (${_OpenMP_LIBRARY} MATCHES "libgomp")
       set(OpenMP_libgomp_LIBRARY ${_OpenMP_LIBRARY}
         CACHE PATH "The OpenMP gomp library.")
-      foreach (lang IN ITEMS C CXX)
-        set(OpenMP_${lang}_LIB_NAMES libgomp)
-        set(OpenMP_${lang}_FLAGS "-fopenmp")
-      endforeach ()
-    elseif (${_OpenMP_LIBRARY} MATCHES "libiomp5md*")
+      set(OpenMP_CXX_LIB_NAMES gomp)
+      set(OpenMP_CXX_FLAGS "-fopenmp")
+      set(OpenMP_gomp_LIBRARY "${_OpenMP_LIBRARY}")
+    elseif (${_OpenMP_LIBRARY} MATCHES "libiomp5md")
       set(OpenMP_libiomp5md_LIBRARY ${_OpenMP_LIBRARY}
         CACHE PATH "The OpenMP iomp5md library.")
-      foreach (lang IN ITEMS C CXX)
-        set(OpenMP_${lang}_LIB_NAMES libiomp5md)
-        set(OpenMP_${lang}_FLAGS "-fopenmp=libiomp5")
-      endforeach ()
+      set(OpenMP_CXX_LIB_NAMES iomp5md)
+      set(OpenMP_CXX_FLAGS "-fopenmp=libiomp5")
+      set(OpenMP_iomp5md_LIBRARY "${_OpenMP_LIBRARY}")
     endif ()
 
     # Let's try this again
-    find_package(OpenMP REQUIRED)
-    foreach (lang IN ITEMS C CXX)
-      set_property(TARGET OpenMP::OpenMP_${lang} APPEND
-        PROPERTY INTERFACE_LINK_LIBRARIES "-L${_OpenMP_LIB_DIR}")
-    endforeach ()
-
+    find_package(OpenMP COMPONENTS CXX)
+    if (OpenMP_CXX_FOUND)
+      if (CMAKE_VERSION VERSION_GREATER 3.13.0)
+        target_link_directories(
+          OpenMP::OpenMP_CXX INTERFACE "${_OpenMP_LIB_DIR}")
+      else ()
+        # This isn't great, but it should work. The better solution is
+        # to use a version of CMake that is at least 3.13.0.
+        set_property(TARGET OpenMP::OpenMP_CXX APPEND
+          PROPERTY INTERFACE_LINK_LIBRARIES "-L${_OpenMP_LIB_DIR}")
+      endif ()
+    endif ()
   endif (NOT _OpenMP_LIBRARY)
-endif (NOT OpenMP_FOUND AND APPLE)
+endif ()
 
 set(_OPENMP_TEST_SOURCE
   "
@@ -65,8 +70,6 @@ include(CheckCXXSourceCompiles)
 set(CMAKE_REQUIRED_FLAGS "${OpenMP_CXX_FLAGS}")
 set(CMAKE_REQUIRED_LIBRARIES OpenMP::OpenMP_CXX)
 check_cxx_source_compiles("${_OPENMP_TEST_SOURCE}" _OPENMP_TEST_COMPILES)
-unset(CMAKE_REQUIRED_FLAGS)
-unset(CMAKE_REQUIRED_LIBRARIES)
 
 get_target_property(_OMP_FLAGS OpenMP::OpenMP_CXX INTERFACE_COMPILE_OPTIONS)
 set_property(TARGET OpenMP::OpenMP_CXX PROPERTY
@@ -81,7 +84,6 @@ else ()
 endif ()
 
 if (EL_HAVE_OPENMP)
-  set(CMAKE_REQUIRED_FLAGS ${OpenMP_CXX_FLAGS})
   set(OMP_COLLAPSE_CODE
     "#include <omp.h>
      int main( int argc, char* argv[] )
@@ -94,14 +96,7 @@ if (EL_HAVE_OPENMP)
          return 0;
      }")
   check_cxx_source_compiles("${OMP_COLLAPSE_CODE}" EL_HAVE_OMP_COLLAPSE)
-  set(CMAKE_REQUIRED_FLAGS)
-else ()
-  set(EL_HAVE_OMP_COLLAPSE FALSE)
-endif ()
 
-# See if we have 'simd' support, which was introduced in OpenMP 4.0
-if (EL_HAVE_OPENMP)
-  set(CMAKE_REQUIRED_FLAGS ${OpenMP_CXX_FLAGS})
   set(OMP_SIMD_CODE
       "#include <omp.h>
        int main( int argc, char* argv[] )
@@ -113,15 +108,10 @@ if (EL_HAVE_OPENMP)
            return 0;
        }")
   check_cxx_source_compiles("${OMP_SIMD_CODE}" EL_HAVE_OMP_SIMD)
-  set(CMAKE_REQUIRED_FLAGS)
-else ()
-  set(EL_HAVE_OMP_SIMD FALSE)
-endif ()
 
-# See if we have 'taskloop' support, which was introduced in OpenMP 4.0
-if (EL_HAVE_OPENMP AND ${PROJECT_NAME}_ENABLE_OMP_TASKLOOP)
-  set(CMAKE_REQUIRED_FLAGS ${OpenMP_CXX_FLAGS})
-  set(OMP_TASKLOOP_CODE
+  # See if we have 'taskloop' support, which was introduced in OpenMP 4.0
+  if (${PROJECT_NAME}_ENABLE_OMP_TASKLOOP)
+    set(OMP_TASKLOOP_CODE
       "#include <omp.h>
        int main( int argc, char* argv[] )
        {
@@ -131,10 +121,19 @@ if (EL_HAVE_OPENMP AND ${PROJECT_NAME}_ENABLE_OMP_TASKLOOP)
                k[i] = i;
            return 0;
        }")
-  check_cxx_source_compiles("${OMP_TASKLOOP_CODE}" HYDROGEN_HAVE_OMP_TASKLOOP)
-  set(CMAKE_REQUIRED_FLAGS)
-  set(${PROJECT_NAME}_ENABLE_OMP_TASKLOOP ${HYDROGEN_HAVE_OMP_TASKLOOP})
+    check_cxx_source_compiles("${OMP_TASKLOOP_CODE}" HYDROGEN_HAVE_OMP_TASKLOOP)
+    set(${PROJECT_NAME}_ENABLE_OMP_TASKLOOP ${HYDROGEN_HAVE_OMP_TASKLOOP})
+  else ()
+    set(HYDROGEN_HAVE_OMP_TASKLOOP FALSE)
+    set(${PROJECT_NAME}_ENABLE_OMP_TASKLOOP FALSE)
+  endif ()
 else ()
   set(HYDROGEN_HAVE_OMP_TASKLOOP FALSE)
-  set(${PROJECT_NAME}_ENABLE_OMP_TASKLOOP FALSE)
-endif ()
+  set(EL_HAVE_OMP_SIMD FALSE)
+  set(EL_HAVE_OMP_COLLAPSE FALSE)
+  set(Hydrogen_ENABLE_OMP_TASKLOOP FALSE)
+endif (EL_HAVE_OPENMP)
+
+set(CMAKE_REQUIRED_FLAGS)
+set(CMAKE_REQUIRED_LIBRARIES)
+set(CMAKE_REQUIRED_LINK_OPTIONS)
