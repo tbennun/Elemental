df5cd365f0d775a906ec27924ed61df46dd65986
Thomas R. Benson <benson31@llnl.gov>
2020-10-02 14:43:05 -0700

    quick patch for Aluminum preprocessor macro usage

--- include/El/core/imports/aluminum.hpp
+++ include/El/core/imports/aluminum.hpp
@@ -162,13 +162,13 @@ using AluminumNCCLBackendIfDefined = Al::NCCLBackend;
 using AluminumNCCLBackendIfDefined = details::BackendNotDefined;
 #endif
 
-#ifdef HYDROGEN_AL_HAVE_HOST_XFER
+#ifdef HYDROGEN_HAVE_AL_HOST_XFER
 using AluminumHostTransferBackendIfDefined = Al::HostTransferBackend;
 #else
 using AluminumHostTransferBackendIfDefined = details::BackendNotDefined;
 #endif
 
-#ifdef HYDROGEN_AL_HAVE_MPI_CUDA
+#ifdef HYDROGEN_HAVE_AL_MPI_CUDA
 using AluminumMPICUDABackendIfDefined = Al::MPICUDABackend;
 #else
 using AluminumMPICUDABackendIfDefined = details::BackendNotDefined;
